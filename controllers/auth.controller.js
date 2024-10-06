7f0801997a34c958c5a45f7eac656e2c6056bce4
willamtessa27 <williamtessa270794@gmail.com>
2024-10-06 16:33:15 +0200

    changing

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -99,7 +99,7 @@ export const logout = (req, res) => {
 
 export const getCurrentUser = async (req, res) => {
 	try {
-		res.json(req.user);
+		return res.json(req.user);
 	} catch (error) {
 		console.error("Error in getCurrentUser controller:", error);
 		res.status(500).json({ message: "Server error" });
