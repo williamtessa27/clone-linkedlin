1dd937202725a8c146f5f51e5b805decb240ab6c
willamtessa27 <williamtessa270794@gmail.com>
2024-10-06 15:52:11 +0200

    settings changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
-
+import bodyParser from "body-parser";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
@@ -18,16 +18,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV !== "production") {
 	app.use(
 		cors({
-			origin: process.env.CLIENT_URL,
+			origin: "https://clone-linkedlin-frontend.vercel.app",
+			// origin: "http://localhost:5173",
 			credentials: true,
 		})
 	);
-}
+
 
 app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json())
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
